48ba2291880dde08e9318e5fd2b0c1e04aa2e91f
Asano2004 <omoti0521@outlook.jp>
2023-06-23 16:30:20 +0900

    6/23

---
+++ 3-06_while/while/main.js
@@ -0,0 +1,13 @@
+// 処理。。
+
+// console.log('while文')
+let i = 1;
+while (i <= 10) {
+    console.log(i + '枚');
+    i += 1;
+}
+
+// console.log('for文')
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
